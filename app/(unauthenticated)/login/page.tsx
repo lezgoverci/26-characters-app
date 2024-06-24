4428253aa43c3ccad5bb82c8ac16ade3077ddbff
Vercillius Mila Jr. <vercilliusjrmila.dev@gmail.com>
2024-06-24 04:27:58 +0000

    fixes apos login

--- app/(unauthenticated)/login/page.tsx
+++ app/(unauthenticated)/login/page.tsx
@@ -49,7 +49,7 @@ export default function Component() {
             </Button>
           </form>
           <div className="text-center text-sm text-muted-foreground">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link href="/signup" className="font-medium underline" prefetch={false}>
               Sign up
             </Link>
